5a735dd4f91a4564e824ba3d3469172d695f7d32
EFetzner <54067091+EFetzner@users.noreply.github.com>
2019-08-13 09:26:22 -0300

    Add files via upload

---
+++ Pizzaria/Pizzaria/Model/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzaria.Model
+{
+    class Pizza
+    {
+        public String Nome { get; set; }
+
+        public String Codigo { get; set; }
+
+        public String Preco { get; set; }
+
+        public String Descricao { get; set; }
+    }
+}
---
+++ Pizzaria/Pizzaria/Program.cs
@@ -0,0 +1,28 @@
+﻿using Pizzaria.Model;
+using System;
+
+namespace Pizzaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = new Pizza();
+            Console.Write("Nome : "); ;
+            pizza.Nome = Console.ReadLine();
+            
+            Console.Write("Codigo : "); ;
+            pizza.Codigo = Console.ReadLine();
+
+            Console.Write("Preco : "); ;
+            pizza.Preco = Console.ReadLine();
+
+            Console.Write("Descricao : "); ;
+            pizza.Descricao = Console.ReadLine();
+
+            Console.WriteLine($"vc escolheu {pizza.Nome} {pizza.Codigo} {pizza.Preco} {pizza.Descricao}");
+
+
+        }
+    }
+}
