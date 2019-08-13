c3fb0fad22d0dcc195e9af249331ff8076b465db
EFetzner <54067091+EFetzner@users.noreply.github.com>
2019-08-13 09:27:44 -0300

    Add files via upload

---
+++ Aula1308/Aula1308/Model/Academico.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula1308.Model
+{
+    class Academico
+    {
+    }
+}
---
+++ Aula1308/Aula1308/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula1308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String nome = Console.ReadLine();
+            Console.WriteLine("vc digitou" + nome);
+        }
+    }
+}
